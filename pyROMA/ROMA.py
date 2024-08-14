8ac743f318bac4fdc0dbbea35e96ec432224a6ae
altyn-bulmers <azhubanchaliyev@gmail.com>
2024-08-14 17:23:02 +0200

    utils

--- pyROMA/ROMA.py
+++ pyROMA/ROMA.py
@@ -3,19 +3,7 @@ import time
 from scipy import stats
 import scanpy as sc
 import multiprocessing
-
-# printing class 
-class color:
-    PURPLE = '\033[95m'
-    CYAN = '\033[96m'
-    DARKCYAN = '\033[36m'
-    BLUE = '\033[94m'
-    GREEN = '\033[92m'
-    YELLOW = '\033[93m'
-    RED = '\033[91m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-    END = '\033[0m'
+from .utils import *
 
 class ROMA:
     def __init__(self):
